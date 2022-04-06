4ae6378736a2ab1bdba0ad4b1a1f70d394388f42
pragyatiwarii123 <pragyatiwari831@gmail.com>
2022-04-06 22:54:19 +0530

    1st change to the into

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const logger = require('./logger')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    console.log('my first request')
+    console.log('the endpoint value is',logger.url)
+    console.log('Calling log function')
+    logger.logging()
     res.send('My first ever api!')
+    console.log(module)
+});
+router.get('/test-me1', function (req, res) {
+    res.send('My second ever api!')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('My forever api!')
 });
 
 module.exports = router;
