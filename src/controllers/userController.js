6302b1dc19d97c89075d7502e304b8ab627bc8ee
pragyatiwarii123 <pragyatiwari831@gmail.com>
2022-04-12 00:20:03 +0530

    my mongo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const UserModel= require("../models/userModel")
 
 const createUser= async function (req, res) {
-    let data= req.body
+    let data= req.body                                  
     let savedData= await UserModel.create(data)
     res.send({msg: savedData})
 }
