f6ceb4ad2d055c62a2c577ff8e53ca4467424391
pragyatiwarii123 <pragyatiwari831@gmail.com>
2022-04-22 11:11:04 +0530

    auth-assignment1

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
 
-        required: true
-    },
+const userSchema = new mongoose.Schema({ 
+    firstName : String,
+    lastName : String,
+    mobile : Number,
     emailId: String,
-    password: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "other"]
-    },
-    age: Number,
-}, { timestamps: true });
+    password : String,
+    gender : String,
+	isDeleted: {
+        type:Boolean,
+        default:false
+    }, //default value is false 
+    age: Number})
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('MyUser', userSchema)
