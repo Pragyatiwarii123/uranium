d72da8d80ab543d22d40d1ceb019e95f329e4f8f
pragyatiwarii123 <pragyatiwari831@gmail.com>
2022-04-24 14:35:36 +0530

    my first change

--- src/controllers/developerController.js
+++ src/controllers/developerController.js
@@ -7,37 +7,38 @@ const createDeveloper = async function (req, res) {
     res.send({ data: developerCreated })
 }
 
-const scholarshipDeveloper = async function (req, res) {
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    //  let developerScholarship = await developerModel.find({percentage:{$gte:70},gender:"Female"})
-    let array = []
-    let developerScholarship = await developerModel.find()
-    for (let i = 0; i < developerScholarship.length; i++) {
-        if (developerScholarship[i].gender === "Female" && developerScholarship[i].percentage >= 70) {
-            arr = array.push(developerScholarship[i])
-        }
-    }
-    // res.send({data: developerScholarship}) 
-    res.send({ msg: array })
+const scholarshipDeveloper = async function(req,res){
+    let scholar =await developerModel.find({gender:"Female",percentage:{$gte:70}})
+    console.log(scholar);
+    res.send({msg:scholar})
 }
 
+// const scholarshipDeveloper = async function (req, res) {
+//     let array = []
+//     let developerScholarship = await developerModel.find()
+//     for (let i = 0; i < developerScholarship.length; i++) {
+//         if (developerScholarship[i].gender === "Female" && developerScholarship[i].percentage >= 70) {
+//             arr = array.push(developerScholarship[i])
+//         }
+//     }
+//     res.send({ msg: array })
+// }
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 const  getDeveloper= async function(req,res){
-    const query1 = req.query    // { percentage: '70', program: 'Radium' }  these are paramas
-    console.log(query1);      
-    let batch1 = await batchModel.find({name:query1.program}).select({_id:1})    //select the id of radium here [{}]
-    console.log(batch1)
-      id1=batch1[0]._id.toString()      // to change objectId to string
-    let developerQuery = await developerModel.find({percentage:{$gte:query1.percentage}, batch:id1})    //now just filter the content
+    const query1 = req.query                
+    console.log(query1);       
+    let batch1 = await batchModel.find({name:query1.program}).select({_id:1})    
+    let developerQuery = await developerModel.find({percentage:{$gte:query1.percentage}, batch:batch1})   
     res.send({msg:developerQuery})
-   
-    // 
 }
 
-
-
-
+// ------------------------------------------------------------------------------------------------------------------------
 
 module.exports.createDeveloper = createDeveloper
 module.exports.scholarshipDeveloper = scholarshipDeveloper
